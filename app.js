c4aa53eeee6831c315c4a549f3ef7d96d591c2fa
pirueto2004 <juliobell2014@gmail.com>
2020-10-12 21:15:36 -0400

    Created new DB in mongodb Atlas. Detached from Heroku Mlab

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
+require("dotenv").config()
+
 const bodyParser = require("body-parser"),
-      mongoose = require("mongoose"),
-      express = require("express"),
-      expressSanitizer = require("express-sanitizer"),
-      app = express(),
-      methodOverride = require("method-override"),
-      DATABASE_NAME = 'restful_blog_app',
-      mongoURI = `mongodb://localhost:27017/${DATABASE_NAME}`;
+  mongoose = require("mongoose"),
+  express = require("express"),
+  expressSanitizer = require("express-sanitizer"),
+  app = express(),
+  methodOverride = require("method-override"),
+  port = process.env.PORT,
+  database_name = process.env.DATABASE_NAME,
+  mongodbPassword = process.env.MONGOPASSWORD,
+  MONGODB_URI = process.env.CONNECTIONSTRING,
+  mongoURI = `mongodb://localhost:27017/${database_name}`
 
 //Set up promises with mongoose
-mongoose.Promise = Promise; 
+mongoose.Promise = Promise
 //if there's a shell environment variable named MONGODB_URI (deployed), use it; otherwise, connect to localhost
-const dbUrl = process.env.MONGODB_URI || mongoURI;
+// const dbUrl = process.env.MONGODB_URI || mongoURI
 // mongoose.connect(MONGOLAB_URI || mongoURI, { useNewUrlParser: true });
-mongoose.connect(dbUrl, { useNewUrlParser: true });
+// mongoose.connect(dbUrl, { useNewUrlParser: true })
+
+//Connecting our app to mongoDB Atlas
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 //Tell Express to use 'body-parser'
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }))
 //Setting the default extension file '.ejs' for all the files that contain the HTML
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(methodOverride("_method"));
-app.use(expressSanitizer());
+app.set("view engine", "ejs")
+// app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"))
+app.use(methodOverride("_method"))
+app.use(expressSanitizer())
 
-const PORT = process.env.PORT || 3000;
-const IP = process.env.IP;
+// const PORT = process.env.PORT || 3000
+const IP = process.env.IP
 
 //SCHEMA SETUP
 const blogSchema = new mongoose.Schema({
-    title: String,
-    image: String,
-    body: String,
-    created: {type: Date, default: Date.now}
-});
+  title: String,
+  image: String,
+  body: String,
+  created: { type: Date, default: Date.now }
+})
 
 //Create the model
-const Blog = mongoose.model("Blog", blogSchema);
+const Blog = mongoose.model("Blog", blogSchema)
 
 //RESTFUL ROUTES
 // Blog.create({
@@ -44,95 +53,95 @@ const Blog = mongoose.model("Blog", blogSchema);
 // });
 
 //ROOT ROUTE
-app.get("/", function(req, res){
-    res.redirect("/blogs");
-});
+app.get("/", function (req, res) {
+  res.redirect("/blogs")
+})
 
 //INDEX ROUTE
-app.get("/blogs", function(req, res){
-    //retrieve all the blogs from the database
-    Blog.find({}, function(err, blogs){
-        if(err) {
-            console.log("ERROR!", err);
-        }else {
-            res.render("index", {blogs: blogs});
-        }
-    });
-});
+app.get("/blogs", function (req, res) {
+  //retrieve all the blogs from the database
+  Blog.find({}, function (err, blogs) {
+    if (err) {
+      console.log("ERROR!", err)
+    } else {
+      res.render("index", { blogs: blogs })
+    }
+  })
+})
 
 //NEW ROUTE
-app.get("/blogs/new", function(req, res){
-    res.render("new");
-});
+app.get("/blogs/new", function (req, res) {
+  res.render("new")
+})
 
 //CREATE ROUTE
-app.post("/blogs", function(req, res){
-    const data = req.body.blog;
-    //create blog
-    console.log(req.body);
-    
-    console.log("===================");
-    console.log(req.body);
-    Blog.create(data, function(err, newBlog){
-        if(err){
-            res.render("new");
-        }else {
-            //then, redirect to the index
-            res.redirect("/blogs");
-        }
-    });
-});
+app.post("/blogs", function (req, res) {
+  const data = req.body.blog
+  //create blog
+  console.log(req.body)
+
+  console.log("===================")
+  console.log(req.body)
+  Blog.create(data, function (err, newBlog) {
+    if (err) {
+      res.render("new")
+    } else {
+      //then, redirect to the index
+      res.redirect("/blogs")
+    }
+  })
+})
 
 //SHOW ROUTE
-app.get("/blogs/:id", function(req, res){
-    Blog.findById(req.params.id, function(err, foundBlog){
-        if(err){
-            res.redirect("/blogs");
-        } else {
-            // res.send("SHOW PAGE!");
-            res.render("show", {blog: foundBlog});
-        }
-    });
-});
+app.get("/blogs/:id", function (req, res) {
+  Blog.findById(req.params.id, function (err, foundBlog) {
+    if (err) {
+      res.redirect("/blogs")
+    } else {
+      // res.send("SHOW PAGE!");
+      res.render("show", { blog: foundBlog })
+    }
+  })
+})
 
 //EDIT ROUTE
-app.get("/blogs/:id/edit", function(req, res){
-    Blog.findById(req.params.id, function(err, foundBlog){
-        if(err){
-            res.redirect("/blogs");
-        } else {
-            res.render("edit", {blog: foundBlog});
-        }
-    });
-});
+app.get("/blogs/:id/edit", function (req, res) {
+  Blog.findById(req.params.id, function (err, foundBlog) {
+    if (err) {
+      res.redirect("/blogs")
+    } else {
+      res.render("edit", { blog: foundBlog })
+    }
+  })
+})
 
 //UPDATE ROUTE
-app.put("/blogs/:id", function(req, res){
-    // Blog.findByIdAndUpdate(id, newData, callback);
-    req.body.blog.body = req.sanitize(req.body.blog.body);
-    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
-        if(err){
-            res.redirect("/blogs");
-        } else {
-            res.redirect("/blogs/" + req.params.id);
-        }
-    });
-});
+app.put("/blogs/:id", function (req, res) {
+  // Blog.findByIdAndUpdate(id, newData, callback);
+  req.body.blog.body = req.sanitize(req.body.blog.body)
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, updatedBlog) {
+    if (err) {
+      res.redirect("/blogs")
+    } else {
+      res.redirect("/blogs/" + req.params.id)
+    }
+  })
+})
 
 //DELETE ROUTE
-app.delete("/blogs/:id", function(req, res){
-    //destroy blog
-    Blog.findByIdAndRemove(req.params.id, function(err){
-        if(err){
-            res.redirect("/blogs");
-        } else {
-            res.redirect("/blogs");
-        }
-    });
-    //redirect somewhere
-});
+app.delete("/blogs/:id", function (req, res) {
+  //destroy blog
+  Blog.findByIdAndRemove(req.params.id, function (err) {
+    if (err) {
+      res.redirect("/blogs")
+    } else {
+      res.redirect("/blogs")
+    }
+  })
+  //redirect somewhere
+})
 
 //Tell Express to listen for requests (start server)
-app.listen(PORT, IP, function(){
-    console.log("The YelpCamp App Server listening on PORT " + PORT);
-});
+app.listen(port, IP, function () {
+  console.log("The RESTfulBlog App Server listening on PORT " + port)
+})
